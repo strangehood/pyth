0b9a64b4ed8a1c59f396d13d3bb9b733f4f05216
strangehood <55440847+strangehood@users.noreply.github.com>
2019-09-24 04:24:00 +0300

    Picture is done

--- 10albatrols.py
+++ 10albatrols.py
@@ -1,18 +1,62 @@
+def bird(x0: int, y0: int):
+    background()
+    nose(0 + x0, -425 + y0)
+    tail(0 + x0 + x0, -420 + y0)
+    wing(60 + x0, -450 + y0)
+    wing(0 + x0, -430 + y0)
+    albatrols(0 + x0, -125 + y0)
+    leg(40 + x0, -475 + y0)
+    leg(0 + x0, -450 + y0)
+
+
 def nose(xnose: int, ynose: int):
-    pass
+    penColor('black')
+    brushColor(255, 230, 128)
+    polygon(([(570 + xnose, 792 + ynose), (571 + xnose, 786 + ynose), (608 + xnose, 780 + ynose),
+              (628 + xnose, 779 + ynose), (643 + xnose, 796 + ynose), (637 + xnose, 793 + ynose)]))
+    polygon(([(569 + xnose, 786 + ynose), (580 + xnose, 790 + ynose), (603 + xnose, 793 + ynose),
+              (633 + xnose, 793 + ynose), (643 + xnose, 790 + ynose), (629 + xnose, 807 + ynose),
+              (565 + xnose, 799 + ynose)]))
+
+
+def tail(xtail: int, ytail: int):
+    penColor('black')
+    brushColor('white')
+    polygon([(162 + xtail, 807 + ytail), (202 + xtail, 713 + ytail), (280 + xtail, 839 + ytail)])
+
+
 def leg(xleg: int, yleg: int):
-    pass
+    feet(xleg, yleg)
+    penSize(40)
+    penColor('white')
+    brushColor('white')
+    line(315 + xleg, 853 + yleg, 358 + xleg, 928 + yleg)
+    penSize(1)
+    circle(315 + xleg, 853 + yleg, 20)
+    circle(358 + xleg, 928 + yleg, 20)
+    penSize(20)
+    line(350 + xleg, 926 + yleg, 428 + xleg, 944 + yleg)
+    penSize(1)
+    circle(428 + xleg, 944 + yleg, 9)
+
 
 def feet(xfeet: int, yfeet: int):
     penColor(0, 0, 0)
     brushColor(255, 230, 128)
-    polygon(([(428 + xfeet, 954 + yfeet), (434 + xfeet, 958 + yfeet), (429 + xfeet, 963 + yfeet), (428 + xfeet, 985 + yfeet),
-              (433 + xfeet, 988 + yfeet), (435 + xfeet, 989 + yfeet), (440 + xfeet, 961 + yfeet), (445 + xfeet, 957 + yfeet),
-              (454 + xfeet, 958 + yfeet), (496 + xfeet, 974 + yfeet), (474 + xfeet, 957 + yfeet), (461 + xfeet, 955 + yfeet),
-              (478 + xfeet, 954 + yfeet), (500 + xfeet, 964 + yfeet), (478 + xfeet, 950 + yfeet), (463 + xfeet, 951 + yfeet),
-              (473 + xfeet, 946 + yfeet), (488 + xfeet, 946 + yfeet), (498 + xfeet, 952 + yfeet), (494 + xfeet, 944 + yfeet),
-              (484 + xfeet, 939 + yfeet), (474 + xfeet, 938 + yfeet), (463 + xfeet, 940 + yfeet), (452 + xfeet, 944 + yfeet),
+    polygon(([(428 + xfeet, 954 + yfeet), (434 + xfeet, 958 + yfeet), (429 + xfeet, 963 + yfeet),
+              (428 + xfeet, 985 + yfeet),
+              (433 + xfeet, 988 + yfeet), (435 + xfeet, 989 + yfeet), (440 + xfeet, 961 + yfeet),
+              (445 + xfeet, 957 + yfeet),
+              (454 + xfeet, 958 + yfeet), (496 + xfeet, 974 + yfeet), (474 + xfeet, 957 + yfeet),
+              (461 + xfeet, 955 + yfeet),
+              (478 + xfeet, 954 + yfeet), (500 + xfeet, 964 + yfeet), (478 + xfeet, 950 + yfeet),
+              (463 + xfeet, 951 + yfeet),
+              (473 + xfeet, 946 + yfeet), (488 + xfeet, 946 + yfeet), (498 + xfeet, 952 + yfeet),
+              (494 + xfeet, 944 + yfeet),
+              (484 + xfeet, 939 + yfeet), (474 + xfeet, 938 + yfeet), (463 + xfeet, 940 + yfeet),
+              (452 + xfeet, 944 + yfeet),
               (442 + xfeet, 949 + yfeet), (438 + xfeet, 946 + yfeet)]))
+
 
 def background():
     brushColor(33, 33, 120)
@@ -39,6 +83,7 @@ def background():
     penColor(0, 102, 128)
     polygon([(0, 550), (790, 550), (790, 1100), (0, 1100)])
 
+
 def wing(xwing: int, ywing: int):  # a,b
     penColor(0, 0, 0)
     brushColor(255, 255, 255)
@@ -58,12 +103,30 @@ def wing(xwing: int, ywing: int):  # a,b
           (116 + xwing, 674 + ywing), (91 + xwing, 664 + ywing), (79 + xwing, 657 + ywing), (68 + xwing, 650 + ywing),
           (60 + xwing, 644 + ywing)]))
 
+
 def albatrols(xalbat: int, yalbat: int):
     penColor(255, 255, 255)
-    c.create_oval(250 + xalbat, 460 + yalbat, 450 + xalbat, 560 + yalbat, fill='white')  # y+300
-    c.create_oval(420 + xalbat, 490 + yalbat, 520 + xalbat, 530 + yalbat, fill='white')
-    c.create_oval(500 + xalbat, 460 + yalbat, 570 + xalbat, 510 + yalbat, fill='white')
-    c.create_oval(540 + xalbat, 480 + yalbat, 555 + xalbat, 490 + yalbat, fill='black')
+    c.create_oval(250 + xalbat, 460 + yalbat, 450 + xalbat, 560 + yalbat, fill='white', outline='white')
+    c.create_oval(420 + xalbat, 490 + yalbat, 520 + xalbat, 530 + yalbat, fill='white', outline='white')
+    c.create_oval(500 + xalbat, 460 + yalbat, 570 + xalbat, 510 + yalbat, fill='white', outline='white')
+    c.create_oval(540 + xalbat, 480 + yalbat, 555 + xalbat, 490 + yalbat, fill='black', outline='white')
+
+
+def main(movement: int):
+    flag = False
+    bird(movement, movement)
+    if flag == True:
+        movement += 5
+    else:
+        movement -= 5
+    if movement > 40:
+        flag = False
+    elif movement < 0:
+        flag = True
+    return movement
+
+def nothing():
+    pass
 
 
 from graph import *
@@ -71,9 +134,8 @@ from graph import *
 c = canvas()
 windowSize(790, 1100)
 canvasSize(790, 1100)
-background()
-albatrols(0, 0)
-wing(0, -450)
-feet(0, -400)
-
+mov=0
+onTimer(nothing, 10)
+main(mov)
+mov = main(mov)
 run()
